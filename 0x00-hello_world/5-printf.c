5b7358f3069e9f17d49ae59560402cd8d4190892
Ntombikayise <Entleh6761@gmail.com>
2023-02-16 15:48:38 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,6 +7,6 @@
  */
 int main(void)
 {
-	print f("with proper grammar, but the outcome is a piece of art,\n");
+	print f("with proper grammar , but the outcome is a piece of art,\n");
 	return (0);
 }
