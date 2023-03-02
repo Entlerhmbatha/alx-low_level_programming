83a70fc4d8de1cef6d7f94330af89868dd78e531
Ntombikayise <Entleh6761@gmail.com>
2023-03-02 14:59:21 -0800

    task 9

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,6 +11,17 @@ char *string_toupper(char *n);
 char *cap_string(char *n);
 char *leet(char *n);
 char *rot13(char *n);
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
 void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
 
 #endif /* MAIN_H */
